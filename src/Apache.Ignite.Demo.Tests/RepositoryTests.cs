40b4b9355d49b4917d36dbddd964ced24561bd58
Pavel Tupitsyn <ptupitsyn@gridgain.com>
2015-11-16 20:54:33 +0300

    Test project added

---
+++ src/Apache.Ignite.Demo.Tests/RepositoryTests.cs
@@ -0,0 +1,19 @@
+﻿using Apache.Ignite.Demo.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Apache.Ignite.Demo.Tests
+{
+    [TestClass]
+    public class RepositoryTests
+    {
+        [TestMethod]
+        public void TestGetPersons()
+        {
+            var repo = Repository.Instance;
+
+            var persons = repo.GetPersons();
+
+            Assert.IsNotNull(persons);
+        }
+    }
+}
